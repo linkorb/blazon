9ce6d76b93afa581d2b95a6618bf035a06a7710e
Joost Faassen <j.faassen@linkorb.com>
2022-04-18 07:36:37 +0100

    fix: sanity checks on blazon document paths

--- src/Model/Document.php
+++ src/Model/Document.php
@@ -2,12 +2,20 @@
 
 namespace Blazon\Model;
 
+use InvalidArgumentException;
+
 class Document
 {
     private $path = null;
 
     public function __construct(string $path, $handler)
     {
+        if ($path[0]!='/') {
+            throw new InvalidArgumentException("Invalid document path. Should start with a slash: " . $path);
+        }
+        if ((substr($path, -1, 1)=='/') && ($path != '/')) {
+            throw new InvalidArgumentException("Invalid document path. No trailing slash allowed: " . $path);
+        }
         $this->path = $path;
         $this->handler = $handler;
     }
